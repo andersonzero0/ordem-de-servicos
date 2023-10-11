3e356eb3666ec62e836f1c2991383ba3b010077a
Anderson Viana <anderson11viana@gmail.com>
2023-10-11 09:38:30 -0300

    update

--- src/components/CardOrdem/index.jsx
+++ src/components/CardOrdem/index.jsx
@@ -1,11 +1,19 @@
 import { Download, Eye } from "lucide-react";
 import "./style.css";
 import { api } from "../../service/api";
-import jsPDF from "jspdf";
-import ReactDOMServer from "react-dom/server";
 import React, { useEffect, useState } from "react";
-import ReactPDF, { PDFDownloadLink } from "@react-pdf/renderer";
-import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
+import { Image, PDFDownloadLink } from "@react-pdf/renderer";
+import {
+  Page,
+  Text,
+  View,
+  Document,
+  StyleSheet,
+  Font,
+} from "@react-pdf/renderer";
+import PoppinsLight from "../../../public/fonts/Poppins/Poppins-Light.ttf";
+import PoppinsMedium from "../../../public/fonts/Poppins/Poppins-Medium.ttf";
+import PoppinsBold from "../../../public/fonts/Poppins/Poppins-Bold.ttf";
 
 export default function CardOrdem({
   name,
@@ -16,199 +24,202 @@ export default function CardOrdem({
   status,
   onClick,
 }) {
-  // const html = (dataForm) => (
-  //   <html>
-  //     <h1 className="titlePDF">ORDEM DE SERVIÇOS</h1>
-
-  //     <div>
-  //       <p className="labelPDF">Nome: {dataForm.name || "Não preenchido"}</p>
-  //       <p className="labelPDF">CPF: {dataForm.document || "Não preenchido"}</p>
-  //       <p className="labelPDF">
-  //         Apelido: {dataForm.nickname || "Não preenchido"}
-  //       </p>
-  //       <p className="labelPDF">
-  //         Solicitador: {dataForm.requester || "Não preenchido"}
-  //       </p>
-  //       <p className="labelPDF">
-  //         Endereço: {dataForm.address || "Não preenchido"}
-  //       </p>
-  //       <p className="labelPDF">
-  //         Celular: {dataForm.phone || "Não preenchido"}
-  //       </p>
-  //       <p className="labelPDF">Email: {dataForm.email || "Não preenchido"}</p>
-  //       <p className="labelPDF">Orçamento: {dataForm.budget ? "Sim" : "Não"}</p>
-  //     </div>
-
-  //     <hr
-  //       style={{
-  //         width: "500px",
-  //         margin: "20px 0",
-  //       }}
-  //     />
-
-  //     <div>
-  //       <p className="labelPDF">Modelo: {dataForm.model || "Não preenchido"}</p>
-  //       <p className="labelPDF">Marca: {dataForm.brand || "Não preenchido"}</p>
-  //       <p className="labelPDF">Placa: {dataForm.plate || "Não preenchido"}</p>
-  //       <p className="labelPDF">Frota: {dataForm.fleet || "Não preenchido"}</p>
-  //       <p className="labelPDF">
-  //         Chassis: {dataForm.chassis || "Não preenchido"}
-  //       </p>
-  //       <p className="labelPDF">
-  //         Renavam: {dataForm.renavam || "Não preenchido"}
-  //       </p>
-  //       <p className="labelPDF">Km: {dataForm.km || "Não preenchido"}</p>
-  //       <p className="labelPDF">Color: {dataForm.color || "Não preenchido"}</p>
-  //       <p className="labelPDF">Ano: {dataForm.age || "Não preenchido"}</p>
-  //       <p className="labelPDF">
-  //         Observação: {dataForm.observation || "Não preenchido"}
-  //       </p>
-  //     </div>
-
-  //     <hr
-  //       style={{
-  //         width: "500px",
-  //         margin: "20px 0",
-  //       }}
-  //     />
-
-  //     <div>
-  //       <p className="labelPDF">
-  //         Problema informado: {dataForm.reported || "Não preenchido"}
-  //       </p>
-  //       <p className="labelPDF">
-  //         Problema constatado: {dataForm.problem_verified || "Não preenchido"}
-  //       </p>
-  //       <p className="labelPDF">
-  //         Serviços realizados: {dataForm.services_performed || "Não preenchido"}
-  //       </p>
-  //     </div>
-
-  //     <hr
-  //       style={{
-  //         width: "500px",
-  //         margin: "20px 0",
-  //       }}
-  //     />
-
-  //     <div>
-  //       <p className="labelPDF">
-  //         NF Serviço: {dataForm.nf_service || "Não preenchido"}
-  //       </p>
-  //       <p className="labelPDF">
-  //         NF Peças: {dataForm.nf_parts || "Não preenchido"}
-  //       </p>
-  //       <p className="labelPDF">
-  //         Faturas: {dataForm.invoices || "Não preenchido"}
-  //       </p>
-  //       <p className="labelPDF">
-  //         Descrição: {dataForm.description_general || "Não preenchido"}
-  //       </p>
-  //     </div>
-
-  //     <hr
-  //       style={{
-  //         width: "500px",
-  //         margin: "20px 0",
-  //       }}
-  //     />
-
-  //     <div>
-  //       <p className="labelPDF">
-  //         Obs. Pneus: {dataForm.obs_wheel || "Não preenchido"}
-  //       </p>
-  //       <p className="labelPDF">
-  //         Obs. Acessórios: {dataForm.obs_accessories || "Não preenchido"}
-  //       </p>
-  //       <p className="labelPDF">
-  //         Obs. Estrutura: {dataForm.obs_structure || "Não preenchido"}
-  //       </p>
-  //       <p className="labelPDF">
-  //         Obs: {dataForm.add_observation || "Não preenchido"}
-  //       </p>
-
-  //       <p className="labelPDF">
-  //         Extra Observação: {dataForm.extra_observation || "Não preenchido"}
-  //       </p>
-  //     </div>
-
-  //     <hr
-  //       style={{
-  //         width: "500px",
-  //         margin: "20px 0",
-  //       }}
-  //     />
-
-  //     <div>
-  //       <p className="labelPDF">
-  //         Preço total: R${dataForm.total_price || "Não preenchido"}
-  //       </p>
-  //       <p className="labelPDF">
-  //         Desconto: R${dataForm.discount || "Não preenchido"}
-  //       </p>
-  //       <p className="labelPDF">
-  //         Forma de pagamento: {dataForm.payment_method || "Não preenchido"}
-  //       </p>
-  //       <p className="labelPDF">
-  //         Total pago: R${dataForm.total_payable || "Não preenchido"}
-  //       </p>
-
-  //       <p className="labelPDF">
-  //         Status: {dataForm.status == "paidout" ? "Pago" : "Pendente"}
-  //       </p>
-  //     </div>
-  //   </html>
-  // );
-
-  // const options = {
-  //   orientation: "portrait",
-  //   format: "a4",
-  // };
   const [order, setOrder] = useState({});
   const [downloadDisable, setDownloadDisable] = useState(true);
 
   useEffect(() => {
     const getOrdem = async () => {
-     const {data} = await api.get(`/order/${id}`)
-      setOrder(data)
-      setDownloadDisable(false)
-    }
-    getOrdem()
-  }, [])
+      const { data } = await api.get(`/order/${id}`);
+      setOrder(data);
+      setDownloadDisable(false);
+    };
+    getOrdem();
+  }, []);
+
+
+
+  Font.register({
+    family: "Poppins",
+    fonts: [
+      { src: PoppinsLight, fontWeight: "light" },
+      { src: PoppinsMedium, fontWeight: "medium" },
+      { src: PoppinsBold, fontWeight: "bold" },
+    ],
+  });
 
   const styles = StyleSheet.create({
     page: {
       flexDirection: "column",
+      fontFamily: "Poppins",
     },
     section: {
       margin: 10,
       padding: 10,
-      backgroundColor: "lightgray"
+      backgroundColor: "#0E213E",
+      borderRadius: "8",
+      flexDirection: "row",
+      gap: "2",
+      flexWrap: "wrap",
+      justifyContent: "space-evenly",
+      alignItems: "center",
     },
     text: {
-      fontWeight: "700"
-    }
+      fontWeight: "ultrabold",
+      fontSize: "16",
+      color: "white",
+      textDecoration: "underline",
+    },
+    section_logo: {
+      margin: "0 auto",
+      textAlign: "center",
+    },
+    img: {
+      width: "50",
+      height: "50",
+    },
   });
 
-  // Create Document Component
-  const MyDocument = ({data}) => {
+  const MyDocument = ({ data }) => {
     return (
-    <Document>
-      <Page size="A4" style={styles.page}>
-        <View style={styles.section}>
-          <Text style={styles.text}>Nome: {data.name}</Text>
-          <Text style={styles.text}>CPF: {data.document}</Text>
-          <Text style={styles.text}>Apelido: {data.nickname}</Text>
-          <Text style={styles.text}>Solicitador: {data.requester}</Text>
-        </View>
-      </Page>
-    </Document>
-  )};
+      <Document>
+        <Page size="A4" style={styles.page}>
+          <View style={styles.section_logo}>
+            <Image src="./Logo.png" style={styles.img} />
+          </View>
+          <View style={styles.section}>
+            <Text style={styles.text}>
+              Nome: {data.name ? data.name : "Não preenchido"}
+            </Text>
+            <Text style={styles.text}>
+              CPF: {data.document ? data.document : "Não preenchido"}
+            </Text>
+            <Text style={styles.text}>
+              Apelido: {data.nickname ? data.nickname : "Não preenchido"}
+            </Text>
+            <Text style={styles.text}>
+              Solicitador: {data.requester ? data.requester : "Não preenchido"}
+            </Text>
+            <Text style={styles.text}>
+              Celular: {data.phone ? data.phone : "Não preenchido"}
+            </Text>
+            <Text style={styles.text}>
+              Email: {data.email ? data.email : "Não preenchido"}
+            </Text>
+            <Text style={styles.text}>
+              Orçamento: {data.budget ? "Sim" : "Não"}
+            </Text>
+          </View>
+          <View style={styles.section}>
+            <Text style={styles.text}>
+              Modelo: {data.model ? data.model : "Não preenchido"}
+            </Text>
+            <Text style={styles.text}>
+              Marca: {data.brand ? data.brand : "Não preenchido"}
+            </Text>
+            <Text style={styles.text}>
+              Placa: {data.plate ? data.plate : "Não preenchido"}
+            </Text>
+            <Text style={styles.text}>
+              Frota: {data.fleet ? data.fleet : "Não preenchido"}
+            </Text>
+            <Text style={styles.text}>
+              Chassis: {data.chassis ? data.chassis : "Não preenchido"}
+            </Text>
+            <Text style={styles.text}>
+              Renavam: {data.renavam ? data.renavam : "Não preenchido"}
+            </Text>
+            <Text style={styles.text}>
+              Km: {data.km ? data.km : "Não preenchido"}
+            </Text>
+            <Text style={styles.text}>
+              Cor: {data.color ? data.color : "Não preenchido"}
+            </Text>
+            <Text style={styles.text}>
+              Ano: {data.age ? data.age : "Não preenchido"}
+            </Text>
+          </View>
+          <View style={styles.section}>
+            <Text style={styles.text}>
+              Problema Informado:{" "}
+              {data.reported ? data.reported : "Não preenchido"}
+            </Text>
+            <Text style={styles.text}>
+              Problema Constatado:{" "}
+              {data.problem_verified ? data.problem_verified : "Não preenchido"}
+            </Text>
+            <Text style={styles.text}>
+              Serviços realizados:{" "}
+              {data.services_performed
+                ? data.services_performed
+                : "Não preenchido"}
+            </Text>
+          </View>
+          <View style={styles.section}>
+            <Text style={styles.text}>
+              NF Serviço: {data.nf_service ? data.nf_service : "Não preenchido"}
+            </Text>
+            <Text style={styles.text}>
+              NF Peças: {data.nf_parts ? data.nf_parts : "Não preenchido"}
+            </Text>
+            <Text style={styles.text}>
+              Faturas: {data.invoices ? data.invoices : "Não preenchido"}
+            </Text>
+          </View>
+          <View style={styles.section}>
+            <Text style={styles.text}>
+              Obs. Pneus: {data.obs_wheel ? data.obs_wheel : "Não preenchido"}
+            </Text>
+            <Text style={styles.text}>
+              Obs. Acessórios:{" "}
+              {data.obs_accessories ? data.obs_accessories : "Não preenchido"}
+            </Text>
+            <Text style={styles.text}>
+              Obs. Estrutura:{" "}
+              {data.obs_structure ? data.obs_structure : "Não preenchido"}
+            </Text>
+            <Text style={styles.text}>
+              Observações:{" "}
+              {data.add_observation ? data.add_observation : "Não preenchido"}
+            </Text>
+            <Text style={styles.text}>
+              Obs. Extra:{" "}
+              {data.extra_observation
+                ? data.extra_observation
+                : "Não preenchido"}
+            </Text>
+          </View>
+          <View style={styles.section}>
+            <Text style={styles.text}>
+              Preço total: R$
+              {data.total_price ? data.total_price : "Não preenchido"}
+            </Text>
+            <Text style={styles.text}>
+              Desconto: R${data.discount ? data.discount : "Não preenchido"}
+            </Text>
+            <Text style={styles.text}>
+              Obs. Estrutura: R$
+              {data.obs_structure ? data.obs_structure : "Não preenchido"}
+            </Text>
+            <Text style={styles.text}>
+              Método:{" "}
+              {data.payment_method ? data.payment_method : "Não preenchido"}
+            </Text>
+            <Text style={styles.text}>
+              Total pago: R$
+              {data.total_payable ? data.total_payable : "Não preenchido"}
+            </Text>
+            <Text style={styles.text}>
+              Pago: R${data.status == "pending" ? "Não" : "Sim"}
+            </Text>
+          </View>
+        </Page>
+      </Document>
+    );
+  };
 
   async function handleDownload() {
     try {
       const { data } = await api.get(`/order/${id}`);
-      //handleExportPDF(data)
     } catch (error) {
       console.log(error);
     }
@@ -242,11 +253,22 @@ export default function CardOrdem({
               onClick={onClick}
             />
           </div>
-          <div className="boxEye" style={downloadDisable ? {
-            touchAction: "none",
-            pointerEvents: "none"
-          } : {}}>
-            <PDFDownloadLink className="boxEye" document={<MyDocument data={order}/>} fileName={`ordem-${id.substring(0, 5)}.pdf`}>
+          <div
+            className="boxEye"
+            style={
+              downloadDisable
+                ? {
+                    touchAction: "none",
+                    pointerEvents: "none",
+                  }
+                : {}
+            }
+          >
+            <PDFDownloadLink
+              className="linkPDF"
+              document={<MyDocument data={order} />}
+              fileName={`ordem-${id.substring(0, 5)}.pdf`}
+            >
               <Download
                 style={{
                   cursor: "pointer",
